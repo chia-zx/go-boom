05756a89dcdfde330db21d3c6a74042bc94734f4
SALLYKZX <124138945+SALLYKZX@users.noreply.github.com>
2023-05-21 01:48:07 +0800

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Deck {
+    private List<String> decks;
+    public static List<String> centerDeck;
+    public static final char[] cardSuits = {'c', 'd', 'h', 's'};
+    public static final char[] cardRanks = {'A', '2', '3', '4', 
+                                            '5', '6', '7', '8', 
+                                            '9', 'X', 'J', 'Q', 'K'};
+    public Deck () {
+        centerDeck = new ArrayList<>();
+        // create and assign value into decks
+        decks = new ArrayList<>();
+        for (int j = 0; j < cardSuits.length; ++j)
+            for (int k = 0; k < cardRanks.length; ++k)
+                decks.add("" + cardSuits[j] + cardRanks[k]);
+    }
+
+    public List<String> getDecks () {
+        return decks;
+    }
+
+    public void shuffleDecks () {
+        Collections.shuffle(decks);
+    }
+
+    public String drawOneCard () {
+        return decks.remove(0);
+    }
+
+    @Override
+    public String toString() {
+        return "Deck\t  : " + getDecks();
+    }
+}
+
+
